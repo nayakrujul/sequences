ac4b4a67df8135cf47032bf283c00a59d420571c
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-10-29 13:35:27 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
-long_description = 'A very concise language - read the docs at https://www.github.com/nayakrujul/thon-symbols'
+long_description = 'A very concise language - read the docs at https://www.github.com/nayakrujul/sequences'
 
 setup(
-  name = 'thon-symbols',
-  version = '1.15',
+  name = 'sequences-lang',
+  version = '1.0',
   license='Apache',
   description = 'A very concise language',
   author = 'Rujul Nayak',
   author_email = 'rujulnayak@outlook.com',
-  url = 'https://github.com/nayakrujul/thon-symbols',
-  download_url = 'https://github.com/nayakrujul/thon-symbols/archive/refs/tags/v114.tar.gz',
+  url = 'https://github.com/nayakrujul/sequences-lang',
+  download_url = 'https://github.com/nayakrujul/sequences-lang/archive/refs/tags/v10.tar.gz',
   keywords = ['golfing', 'code-golf', 'language'],
   install_requires=[
       ],
@@ -32,7 +32,7 @@ setup(
   packages = find_packages(),
   entry_points = {
     'console_scripts': [
-      'thonsymbols = thon_symbols.main:from_cmdline'
+      'sequences = sequences.main:from_cmdline'
     ]
   }
 )
