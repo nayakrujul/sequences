72acd64349966b5b5d67de3c01919c78e21a476a
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-11-04 09:38:53 +0000

    Update List.py

--- src/sequences/List.py
+++ src/sequences/List.py
@@ -14,11 +14,11 @@ class List(list):
     def first(self, types, b=True, n=1):
         f = self._first(types, b, n)
         while len(f) < n:
-            if (types == str) or (str in types):
+            if (types == str) or (isinstance(types, type) and (str in types)):
                 i = input()
                 f.insert(0, i)
                 self.push(i)
-            elif (types == list) or (list in types):
+            elif (types == list) or (isinstance(types, type) and (list in types)):
                 i = input().split()
                 f.insert(0, i)
                 self.push(i)
