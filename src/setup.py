55da1293e763bc7369be493b44658e1b35135b72
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-11-04 09:40:41 +0000

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ long_description = 'A very concise language - read the docs at https://www.githu
 
 setup(
   name = 'sequences-lang',
-  version = '1.6',
+  version = '1.7',
   license='Apache',
   description = 'A very concise language',
   author = 'Rujul Nayak',
